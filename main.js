cea8d2f88cd7f93bca563f34bb8f42fb2d8b87b8
MramirhoseinS <115915475+MramirhoseinS@users.noreply.github.com>
2023-12-22 12:40:58 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf')
 const { message } = require('telegraf/filters')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.6124548441:AAFg49l11HsWoKjQkWAlXuhmb9BTAxRNoHM)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
